aec862e3caa188604faee42517791d1fb980cc7c
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-03-07 20:56:35 +0530

    basic javascript

--- first-app.js
+++ first-app.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+//console.log('Hello World')
+const multiply=(a,b)=>{
+    return a*b;
+}
+console.log(multiply(2,5));
+
+const student={
+    name:'Dhruv',
+    age:21,
+    intro(){
+        console.log('Hi I am '+this.name)
+    }
+}
+student.intro()
