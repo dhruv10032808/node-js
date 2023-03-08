c88ed9134d13a8b952015ce57de1311bf5645d1f
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-03-08 17:10:42 +0530

    returning responses based on url

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const http = require("http");
 
-const server=http.createServer((request,response)=>{
-    console.log('Dhruv')
+const server=http.createServer((req,res)=>{
+    let url=req.url;
+    if(url==='/home'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>')
+        res.write('<body><h1>Welcome home</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if(url==='/about'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>')
+        res.write('<body><h1>Welcome to About Us page</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }
+    if(url==='/node'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>')
+        res.write('<body><h1>Welcome to Node js project</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }
 })
 server.listen(4000)
