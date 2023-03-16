e77cbf7dfb242ef7e4fdc4ab763667621fd5c055
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-03-16 16:21:51 +0530

    express js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const http = require("http");
-const routes=require('./routes')
-const server=http.createServer(routes.handler)
-server.listen(3000);
+const express=require("express");
+const app=express();
+app.use((req,res,next)=>{
+    console.log("1st middleware")
+    next();
+})
+app.use((req,res,next)=>{
+    console.log('2nd middleware');
+    res.send('<h1>Welcome!</h1>');
+})
+app.listen(3000)
